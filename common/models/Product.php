ec6a1569f80c13f4c5a065d5d557dafb74ff964d
AnaKarolina52 <60392924+AnaKarolina52@users.noreply.github.com>
2021-03-08 19:34:00 +0000

    ActiveRecord models

---
+++ common/models/Product.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%products}}".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property string|null $image
+ * @property float $price
+ * @property int $status
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ * @property int|null $created_by
+ * @property int|null $update_by
+ *
+ * @property CartItems[] $cartItems
+ * @property OrdersItems[] $ordersItems
+ * @property User $createdBy
+ * @property User $updateBy
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%products}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'price', 'status'], 'required'],
+            [['description'], 'string'],
+            [['price'], 'number'],
+            [['status', 'created_at', 'updated_at', 'created_by', 'update_by'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+            [['image'], 'string', 'max' => 2000],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
+            [['update_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['update_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'description' => 'Description',
+            'image' => 'Image',
+            'price' => 'Price',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'created_by' => 'Created By',
+            'update_by' => 'Update By',
+        ];
+    }
+
+    /**
+     * Gets query for [[CartItems]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\CartItemsQuery
+     */
+    public function getCartItems()
+    {
+        return $this->hasMany(CartItems::className(), ['product_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[OrdersItems]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\OrdersItemsQuery
+     */
+    public function getOrdersItems()
+    {
+        return $this->hasMany(OrdersItems::className(), ['product_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\UserQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+
+    /**
+     * Gets query for [[UpdateBy]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\UserQuery
+     */
+    public function getUpdateBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'update_by']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\ProductQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\ProductQuery(get_called_class());
+    }
+}
